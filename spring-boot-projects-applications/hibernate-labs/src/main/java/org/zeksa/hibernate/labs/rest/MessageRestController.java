9df861762e868657f218b77d723f07f4c3a4fc77
root <root@localhost.localdomain>
2016-01-16 03:10:13 +0200

    added first rest resources

---
+++ spring-boot-projects-applications/hibernate-labs/src/main/java/org/zeksa/hibernate/labs/rest/MessageRestController.java
@@ -0,0 +1,9 @@
+package org.zeksa.hibernate.labs.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/message")
+public class MessageRestController {
+}
