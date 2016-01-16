35a42b97263c92fdabdf9e34f291e24615909de5
root <root@localhost.localdomain>
2016-01-16 03:12:57 +0200

    added first rest resources

--- spring-boot-projects-servers/hibernate-labs-appserver/src/main/java/org/zeksa/hibernate/labs/AppServer.java
+++ spring-boot-projects-servers/hibernate-labs-appserver/src/main/java/org/zeksa/hibernate/labs/AppServer.java
@@ -1,18 +1,11 @@
 package org.zeksa.hibernate.labs;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
 @SpringBootApplication
-@EnableAutoConfiguration
-@ComponentScan
 public class AppServer {
-
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(AppServer.class, args);
 	}
---
+++ spring-boot-projects-servers/hibernate-labs-appserver/src/main/java/org/zeksa/hibernate/labs/AppServerConfiguration.java
@@ -0,0 +1,11 @@
+package org.zeksa.hibernate.labs;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class AppServerConfiguration {
+}
