d26e6db300bacfa9e204f3ce4ad52d978d8b301f
root <root@localhost.localdomain>
2016-01-16 03:45:41 +0200

    added JPA annotations

--- spring-boot-projects-applications/hibernate-labs/hibernate-labs-model/src/main/java/org/zeksa/hibernate/labs/model/Message.java
+++ spring-boot-projects-applications/hibernate-labs/hibernate-labs-model/src/main/java/org/zeksa/hibernate/labs/model/Message.java
@@ -1,6 +1,20 @@
 package org.zeksa.hibernate.labs.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Message {
 
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	@Getter
+	@Setter
 	private String text;
 }
