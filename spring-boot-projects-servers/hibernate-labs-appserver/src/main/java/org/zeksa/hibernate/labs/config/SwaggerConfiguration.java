271ab8e0d4bb64fa255a59f91d5759f6cc1ecd01
vyacheslav.mysak <vyacheslav.mysak@engagepoint.com>
2016-08-29 14:27:49 +0300

    fixed swagger build

--- spring-boot-projects-servers/hibernate-labs-appserver/src/main/java/org/zeksa/hibernate/labs/config/SwaggerConfiguration.java
+++ spring-boot-projects-servers/hibernate-labs-appserver/src/main/java/org/zeksa/hibernate/labs/config/SwaggerConfiguration.java
@@ -5,32 +5,35 @@ import com.mangofactory.swagger.models.dto.ApiInfo;
 import com.mangofactory.swagger.plugin.EnableSwagger;
 import com.mangofactory.swagger.plugin.SwaggerSpringMvcPlugin;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
 
 @Configuration
 @EnableSwagger
 public class SwaggerConfiguration {
 
-	private SpringSwaggerConfig springSwaggerConfig;
+    private SpringSwaggerConfig springSwaggerConfig;
 
-	@Autowired
-	public void setSpringSwaggerConfig(SpringSwaggerConfig springSwaggerConfig) {
-		this.springSwaggerConfig = springSwaggerConfig;
-	}
+    @Autowired
+    public void setSpringSwaggerConfig(SpringSwaggerConfig springSwaggerConfig) {
+        this.springSwaggerConfig = springSwaggerConfig;
+    }
 
-	@Bean
-	public SwaggerSpringMvcPlugin customImplementation() {
-		return new SwaggerSpringMvcPlugin(this.springSwaggerConfig).apiInfo(
-				apiInfo()).includePatterns("/api/.*");
-	}
+    @Bean
+    public SwaggerSpringMvcPlugin customImplementation() {
+        return new SwaggerSpringMvcPlugin(this.springSwaggerConfig).apiInfo(
+                apiInfo()).includePatterns("/api/.*");
+    }
 
-	private ApiInfo apiInfo() {
-		ApiInfo apiInfo = new ApiInfo("Hibernate Labs Project API", "API for Hibernate Labs Project",
-				"Licensed under the Apache License, Version 2.0", "",
-				"Apache License, Version 2.0", "https://www.apache.org/licenses/LICENSE-2.0");
-		return apiInfo;
-	}
+    private ApiInfo apiInfo() {
+        ApiInfo apiInfo = new ApiInfo("Hibernate Labs Project API", "API for Hibernate Labs Project",
+                "Licensed under the Apache License, Version 2.0", "",
+                "Apache License, Version 2.0", "https://www.apache.org/licenses/LICENSE-2.0");
+
+        return apiInfo;
+    }
 
 }
